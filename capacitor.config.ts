d4baeae53fef8db88219ca091632afce890cdeac
axlopezre <83840788+axlopezre@users.noreply.github.com>
2022-11-16 22:31:23 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ExU3OverTime',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
